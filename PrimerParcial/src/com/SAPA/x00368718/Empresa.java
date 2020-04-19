3d9994820e6681f7d9a71866e05fbfbbf1bc62c2
Salvador-Pocasangre-UCA <54720654+Salvador-Pocasangre-UCA@users.noreply.github.com>
2020-04-19 17:10:03 -0600

    Fixing error in quitEmpleado method

--- PrimerParcial/src/com/SAPA/x00368718/Empresa.java
+++ PrimerParcial/src/com/SAPA/x00368718/Empresa.java
@@ -46,7 +46,7 @@ public class Empresa {
         if (!planilla.removeIf(obj->{
             return (obj.getNombre()).equalsIgnoreCase(nombre);
         })) {
-            throw new NotExistingEmployedException("El empleado" + nombre + "no existe");
+            throw new NotExistingEmployedException("El empleado: " + EmpleadoDespe + " no existe");
         }
     }
     public void consultarEmpleados(){
